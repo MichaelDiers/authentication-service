7620a64413e824bda721021c7fe75cc82e7897c9
MichaelDiers <md@sreid.de>
2022-10-05 17:50:15 +0200

    deploy to google cloud run

--- authentication/config/production_config.py
+++ authentication/config/production_config.py
@@ -11,8 +11,4 @@ class ProductionConfig(Config):
     '''
         The base configuration of the application.
     '''
-    API_KEY = 'API_KEY_PRODUCTION'
-    JWT_ALGORITHM = 'HS256'
-    JWT_SECRET = 'secret'
     TESTING = False
-    USER_SERVICE_URL = 'https://api-gateway-prdsggicqa-uc.a.run.app/api/v1/users-service'
